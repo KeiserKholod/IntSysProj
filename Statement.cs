48002669f22a6f45ed2a8d7033eff0b66a6d23b2
Broky-Dotnet <aleksejsergej829@gmail.com>
2023-11-26 18:04:35 +0500

    Проба

---
+++ Statement.cs
@@ -0,0 +1,18 @@
+﻿namespace ProcessingTextFile
+{
+    public class Statement
+    {
+        public string StatementA;
+        public string StatementB;
+
+        public string OperandType;
+
+        /// <summary>
+        /// Возвращает строковое представление условия.
+        /// </summary>  
+        public string StringRepresentation
+        {
+            get => StatementA + OperandType + StatementB;
+        }
+    }
+}
